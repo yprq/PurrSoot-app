91dac759b9bdf887ad97b9930608a9c2c879b8c9
yprq <beremyaprak@gmail.com>
2026-05-03 17:43:05 +0300

    tips, homeview changes

---
+++ pursootapp-ios/pursootapp-ios/Models/Tips.swift
@@ -0,0 +1,8 @@
+//
+//  Tips.swift
+//  pursootapp-ios
+//
+//  Created by Yaprak Aslan on 3.05.2026.
+//
+
+import Foundation
---
+++ pursootapp-ios/pursootapp-ios/TipDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  TipDetailsView.swift
+//  pursootapp-ios
+//
+//  Created by Yaprak Aslan on 3.05.2026.
+//
+
+import SwiftUI
+
+struct TipDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TipDetailsView()
+}
